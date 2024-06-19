aba13612c033c12e29752707b8cbeaa6374c695b
kochi-echo <tnodtegrof@gmail.com>
2024-06-19 10:59:47 +0900

    enに一本化

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
-    config.i18n.default_locale = "ja" # "ja" or "en-US"
+    config.i18n.default_locale = :ja # :ja or :en
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
